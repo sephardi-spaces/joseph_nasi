4f2411a4ab9a66623fead6936ef4f8ad7e274927
Sephardi Spaces <sepharadspaces@gmail.com>
2025-10-28 16:48:10 -0400

    Update Google Sheets URL in google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1IlwPmIzNaFhAEUmSTLDVqbtJiUJlb7qcgy8lf1wyw4o/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xZmLSbjYkg9LKt3E9rC1jzonDmy6l_hm/edit?usp=sharing&ouid=100551796678231143064&rtpof=true&sd=true';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
